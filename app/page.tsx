4f9ad853b81990afd88ab9ef69ff1ccdc0176c40
uchihori <show2305@gmail.com>
2023-12-06 21:17:38 +0900

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,7 @@ export default async function Home() {
   return (
     <main>
       <h1>UUID占い</h1>
-      <div>
-        UUID(Universally Unique
-        IDentifier)は340,282,366,920,938,463,463,374,607,431,768,211,456通りの可能性がある文字列です。
-        人の選択したUUIDと重複する可能性は殆どありません。つまり、あなたがUUIDを選択することで、星占いや血液型占いよりもパーソナライズされた占いを行うことができます。
-      </div>
+      <div>あなたが生成したUUIDに基づいて今日の運勢を占います。</div>
       <UUID prevResult={prevResult} fortune={fortune} />
     </main>
   );
